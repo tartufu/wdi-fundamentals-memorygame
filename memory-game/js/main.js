14bbeac8e3b8775df0134ec8c09a947ad084fcc0
Tartufu <tartufu@users.noreply.github.com>
2019-02-16 12:31:01 +0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,9 +1,21 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log(cardsInPlay);
 console.log("User flipped" + " " + cardOne);
 console.log("User flipped" + " " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardOne === cardTwo) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry try again!")
+	};
+
+};
