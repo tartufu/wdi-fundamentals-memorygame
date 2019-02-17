eca55857fab2388c89f4bed190ef4ac3cd9f40dc
Tartufu <tartufu@users.noreply.github.com>
2019-02-17 16:37:30 +0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -3,19 +3,20 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log(cardsInPlay);
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardOne === cardTwo) {
-		alert("You found a match!");
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!")
 	} else {
-		alert("Sorry try again!")
-	};
+		console.log("Sorry, try again!")
+	}
+}
 
+var flipCard = function(cardId) {
+	//
+	console.log(`User flipped ${cards[cardId]}`);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
 };
+
+flipCard(0);
+flipCard(2);
